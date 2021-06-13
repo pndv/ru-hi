9abad096fd750d39bdcb62d78c203a014e6d6eb8
Vinay Pandey <babbupandey@protonmail.com>
2021-06-13 00:33:18 -0700

    Add word endings for nouns Reformat tables for consistency Add run scripts for Windows and *nix

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo on
+
+xelatex -aux-directory=out -output-directory=out -synctex=1 -interaction=nonstopmode -enable-installer main.tex
+biber --output-directory out --isbn13 --isbn-normalise main
+makeindex out/main.idx
+
+rem Run XeLaTeX two more times to include the generated index, bibliography and tables
+xelatex -aux-directory=out -output-directory=out -synctex=1 -interaction=nonstopmode -enable-installer main.tex
+xelatex -aux-directory=out -output-directory=out -synctex=1 -interaction=nonstopmode -enable-installer main.tex
