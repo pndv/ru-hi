49add9783021ccfb8bad21271ec31f2bb2e8f121
Vinay Pandey <babbupandey@protonmail.com>
2021-06-17 16:58:45 -0700

    Add noun forms

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo on
 
-xelatex -aux-directory=out -output-directory=out -synctex=1 -interaction=nonstopmode -enable-installer main.tex
+xelatex -aux-directory=out -output-directory=out -interaction=nonstopmode -enable-installer main.tex
+xelatex -aux-directory=out -output-directory=out -interaction=nonstopmode -enable-installer main.tex
 biber --output-directory out --isbn13 --isbn-normalise main
 makeindex out/main.idx
 
