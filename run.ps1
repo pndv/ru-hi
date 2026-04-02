50b2bc70eba6b0c5422a020b816b59b32ec67651
Vinay Pandey <babbupandey@protonmail.com>
2026-04-01 19:42:20 -0700

    Fixes  (#10)
    * small fixes
    
    * Improve formatting, fix typos, and update file references
    
    Corrected quote styles in text, fixed errors in Cyrillic transliteration, and resolved inconsistencies in file paths/references. Also renamed a script file for better clarity.
    
    * Improve `run.ps1` script with detailed log messages and fix a minor typo in `tenses.tex`

--- run.ps1
+++ run.ps1
@@ -1,10 +1,18 @@
-@echo on
+Write-Output "Running XeLaTeX first time"
+xelatex -aux-directory=out -output-directory=out -interaction=nonstopmode -enable-installer main.tex
 
+Write-Output "Running XeLaTeX second time"
 xelatex -aux-directory=out -output-directory=out -interaction=nonstopmode -enable-installer main.tex
-xelatex -aux-directory=out -output-directory=out -interaction=nonstopmode -enable-installer main.tex
+
+Write-Output "Running biber for bibliography"
 biber --output-directory out --isbn13 --isbn-normalise main
+
+Write-Output "Generating index"
 makeindex out/main.idx
 
-rem Run XeLaTeX two more times to include the generated index, bibliography and tables
+# Run XeLaTeX two more times to include the generated index, bibliography and tables
+Write-Output "Running XeLaTeX third time to include the generated index"
 xelatex -aux-directory=out -output-directory=out -synctex=1 -interaction=nonstopmode -enable-installer main.tex
+
+Write-Output "Running XeLaTeX fourth time to include the generated indices, bibliography, tables etc."
 xelatex -aux-directory=out -output-directory=out -synctex=1 -interaction=nonstopmode -enable-installer main.tex
